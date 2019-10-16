973cb64b5312582d0d346d8f17b1190c931239eb
Codebrother1 <terrance.cazy@me.com>
2019-10-16 13:40:57 -0500

    Scaffold the App

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = () => {
+    this.setState({ term: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
